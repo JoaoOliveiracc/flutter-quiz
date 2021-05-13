cc4dd1027c77d00619d451610ab2c1a92db51a05
João Monteiro <ccjoao_monteiro@outlook.com>
2021-05-13 12:40:45 -0300

    Using StatefulWidget

---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+main() => runApp(new PerguntaApp());
+
+class PerguntaAppState extends State<PerguntaApp> {
+  var perguntaSelecionada = 0;
+
+  void responder() {
+    setState(() {
+      perguntaSelecionada++;
+    });
+    print(perguntaSelecionada);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    final perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual o seu animal favorito?'
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text(perguntas[perguntaSelecionada]),
+            RaisedButton(
+              child: Text('Resposta 1'),
+              onPressed: responder,
+            ),
+            RaisedButton(
+              child: Text('Resposta 2'),
+              onPressed: responder,
+            ),
+            RaisedButton(
+              child: Text('Resposta 3'),
+              onPressed: responder,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PerguntaApp extends StatefulWidget{
+  @override
+  PerguntaAppState createState() {
+    return PerguntaAppState();
+  }
+
+}
