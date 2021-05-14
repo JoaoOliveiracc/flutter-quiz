499433653aa269a308a7a5deafb2568ff4586b4d
João Monteiro <ccjoao_monteiro@outlook.com>
2021-05-14 12:13:55 -0300

    Correcting invalid question error

--- lib/main.dart
+++ lib/main.dart
@@ -6,49 +6,51 @@ import './answer.dart';
 main() => runApp(new PerguntaApp());
 
 class _PerguntaAppState extends State<PerguntaApp> {
-  var _perguntaSelecionada = 0;
+  var _selectedQuestion = 0;
+
+  final _questions = const [
+    {
+      'text': 'Qual é a sua cor favorita?',
+      'answer': ['Preto', 'Vermelho', 'Verde', 'Branco'],
+    },
+    {
+      'text': 'Qual é o seu animal favorito?',
+      'answer': ['Elefante', 'Leão', 'Coelho', 'Cobra'],
+    },
+    {
+      'text': 'Qual time você torce?',
+      'answer': ['São Paulo', 'Flamengo', 'Palmeiras', 'Internacioanl'],
+    }
+  ];
 
   void _answer() {
     setState(() {
-      _perguntaSelecionada++;
+      _selectedQuestion++;
     });
+  }
+
+  bool get selectedQuestion {
+    return _selectedQuestion < _questions.length;
   }
 
   @override
   Widget build(BuildContext context) {
-
-    final List<Map<String, Object>> perguntas = [
-      {
-        'text': 'Qual é a sua cor favorita?',
-        'answer': ['Preto', 'Vermelho', 'Verde', 'Branco'],
-      },
-      {
-        'text': 'Qual é o seu animal favorito?',
-        'answer': ['Elefante', 'Leão', 'Coelho', 'Cobra'],
-      },
-      {
-        'text': 'Qual time você torce?',
-        'answer': ['São Paulo', 'Flamengo', 'Palmeiras', 'Internacioanl'],
-      }
-    ];
-
-    List<Widget> answers = [];
-
-    for(var textAnswer in perguntas[_perguntaSelecionada]['answer']) {
-      answers.add(Answer(textAnswer, _answer));
-    }
+    
+    List<String> answers = selectedQuestion
+    ?_questions[_selectedQuestion]['answer']
+    :null;
 
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Perguntas'),
         ),
-        body: Column(
+        body: selectedQuestion ? Column(
           children: <Widget>[
-            Question(perguntas[_perguntaSelecionada]['text']),
-            ...answers,
+            Question(_questions[_selectedQuestion]['text']),
+            ...answers.map((text) => Answer(text, _answer)).toList(),
           ],
-        ),
+        ) : null,
       ),
     );
   }
